91eb2a87286916a34f2986a501456310f517e8ac
Pedro Lisboa <lisboa.ti.mkt@gmail.com>
2023-01-21 21:47:37 -0300

    Remove

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,7 +1,5 @@
-import { SearchNormal1 } from 'iconsax-react'
 import Logo from '../../assets/marvelLogo.webp'
 import { HeaderPageStyled } from './style'
-import { Link, animateScroll as scroll } from "react-scroll";
 
 export const HeaderPage = () => {
 
